7e649e048a3fc626ef800b8f47cead7dc3be6d32
Aryanmahakur <mahakuraryan@gmail.com>
2026-01-29 10:50:39 +0530

    uf

--- unit1.java
+++ unit1.java
@@ -1,5 +1,16 @@
-class Solution {
-    public boolean canWinNim(int n) {
-        return n % 4 != 0;
+
+class unit1 {
+    static void printNumbers(int... numbers) {
+
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printNumbers(1, 2);
+        printNumbers(10, 20, 30);
+        printNumbers(); // valid
     }
 }
