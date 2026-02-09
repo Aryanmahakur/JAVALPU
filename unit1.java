db4a0108b61a9b3cf545d161695bb9403f674017
Aryanmahakur <mahakuraryan@gmail.com>
2026-02-09 23:22:12 +0530

    uf

--- unit1.java
+++ unit1.java
@@ -1,29 +1,9 @@
-import java.util.Scanner;
-
 public class unit1 {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        if (n <= 1) {
-           
-            return;
+     int[]arr={1,20,3,1,4};
+        for (int i = 1; i <= 5; i++) {
+ 
         }
-
-        boolean isPrime = true;
-
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime)
-            System.out.println(n + " is a p number");
-        else
-            System.out.println(n + " is NOT a p number");
     }
 }
+D
